ca5c6ab09c9eaf75b402797fdbc01fb304febf05
recife25 <felipe.j.ruiz64@gmail.com>
2020-11-22 21:16:44 -0500

    Added Static Keyword

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿//Felipe Ruiz
+//Static Exercises
+using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var celcius = TempConverter.FahrenheitToCelsius(68);
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(20);
+            Console.WriteLine("After Conversions");
+            Console.WriteLine($"celcius{celcius}");
+            Console.WriteLine("");
+            Console.WriteLine($"farenheit{fahrenheit}");
+            
+        }
+    }
+}
---
+++ TempConverter.cs
@@ -0,0 +1,23 @@
+﻿//Felipe Ruiz
+//StaticKeyword
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+       public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            //return (fahrenheit - 32) / 1.8;
+            var result = (fahrenheit - 32) / 1.8;
+            return result;
+        }
+        
+       public static double CelsiusToFahrenheit(double celsius)
+        {
+            return (celsius * 9) / 5 + 32;
+        }
+    }
+}
